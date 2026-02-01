38e19356db0826bdf73d81781784a8e35edcfcec
Adán <faolivab@gmail.com>
2026-02-01 21:03:04 +0100

    Configuration de astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://adanoliva.github.io', 
+  base: '/personal-web', 
 });
